9681a707ef2a3d83495cefe8f1208462538ae5a9
C-Sharp-Yassine <153088430+C-Sharp-Yassine@users.noreply.github.com>
2026-09-09 23:16:54 +0200

    feat: configure CORS for frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import cors from "cors";
 import ticketsRouter from "./routes/tickets.js";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173"
+})
+);
 
 app.use(express.json());
 
